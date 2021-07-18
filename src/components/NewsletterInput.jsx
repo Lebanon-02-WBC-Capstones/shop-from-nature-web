ef3e35a4cc0e92f93112fb184ebb42535bf833be
MiraChami <Miramc149@gmail.com>
2021-07-18 18:45:08 +0300

    finished newsletter

--- src/components/NewsletterInput.jsx
+++ src/components/NewsletterInput.jsx
@@ -9,15 +9,21 @@ function NewsletterInput() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className=" flex flex-row space-x-3 ml-7 tracking-wide "
+    >
       <input
         type="text"
         value={email}
         placeholder="Enter your email"
+        className="text-black bg-gray w-45 py-1.5 px-1 placeholder-dark border-none rounded "
         onChange={(e) => setEmail(e.target.value)}
         required
       />
-      <button className="btn btn-blue">Subscribe</button>
+      <button className="btn bg-primary text-base p-3 rounded">
+        Subscribe!
+      </button>
     </form>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      primary: "#5F7264",
+      primary: "#7eac8a",
       secondary: "#9F6544",
+      black: "#000000",
+      gray: "#c5cbc5",
+      dark: "#717771",
+      tan: "#fcfbf9",
     },
     extend: {},
   },
