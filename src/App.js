1b1228362b9fdede74dfd2bafd85987cee03f36e
MiraChami <Miramc149@gmail.com>
2021-07-20 22:27:17 +0300

    first draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Newsletter from "./components/Newsletter";
+import OurValues from "./components/OurValues";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <OurValues />
         <Newsletter />
       </header>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,15 @@ module.exports = {
       primary: "#7eac8a",
       secondary: "#9F6544",
       black: "#000000",
+      white:"#fcfbfb",
       gray: "#c5cbc5",
       dark: "#717771",
       tan: "#fcfbf9",
+      bric: "#892E2E",
     },
+    fontFamily: {
+      "oswald": "Oswald",
+     },
     extend: {},
   },
   variants: {
