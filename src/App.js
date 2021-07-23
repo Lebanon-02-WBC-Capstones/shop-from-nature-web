edcaeb0adec9a6dc0db99c24087c42a0206c0827
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-07-23 20:47:18 +0300

    first step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import React from "react";
-
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to the capstone projects, this is your initial react app</p>
       </header>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Footer = (props) => {
-  return <div>this is the Footer components</div>;
+// import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <footer className="flex items-center min-h-quarter">
+      {/* <Link to= */}
+      <p className="text-red text-opacity-60 text-left pl-1">
+        &copy; LivingVert
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
