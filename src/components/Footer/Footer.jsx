6fa218edef9ef39d80ebe9f144c63ccb45abbdcd
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-07-25 11:00:42 +0300

    footer-component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import SocialMediaIcons from "../../Icons/SocialMediaIcons";
 const Footer = () => {
   return (
-    <footer className="flex items-center min-h-quarter">
-      {/* <Link to= */}
-      <p className="text-red text-opacity-60 text-left pl-1">
-        &copy; LivingVert
-      </p>
+    <footer className="flex items-center min-h-small bg-transparent justify-between mt-1">
+      <Link to="/">
+        <p className="text-red text-lg text-opacity-60 pl-3 pl-1">
+          &copy; 2021LivingVert
+        </p>
+      </Link>
+      <div className="pr-2 text-lg flex items-center">
+        <Link to="/shop">
+          <p className="text-olivegreen text-md pr-6 font-medium">Shop</p>
+        </Link>
+        <Link to="/blog">
+          <p className="text-olivegreen text-md font-medium pr-6">Blog</p>
+        </Link>
+        <Link to="/about">
+          <p className="text-olivegreen text-md font-medium pr-6">About</p>
+        </Link>
+      </div>
+      <div className="pr-4">
+        <SocialMediaIcons />
+      </div>
     </footer>
   );
 };
