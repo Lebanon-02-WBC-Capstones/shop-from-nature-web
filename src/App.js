272576e750c82e5086d412d8cf16d6815ab005fb
osamaakb <osama98akb@gmail.com>
2021-07-25 11:48:10 +0300

    fix: navbar import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
