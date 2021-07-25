dd1991289848fac0631a0eda2d041f041f063136
MiraChami <Miramc149@gmail.com>
2021-07-25 21:42:54 +0300

    fix import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import React from "react";
 import OurValues from "./components/OurValues/OurValues";
-import Newsletter from "./components/Newsletter/Newsletter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <OurValues />
-        <Newsletter />
       </header>
     </div>
   );
