0d0d0eb1275581be276dc1f3a4a9107489d506fe
Cesar Maaz <cesarmaaz@gmail.com>
2021-07-26 18:03:11 +0300

    <Feat>: <recent blog item>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import "./App.css";
 import React from "react";
+import RecentBlogItem from "./components/HomePage/RecentBlogItem";
+//import BambooStraw from "./Images/BambooStraw";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
-      </header>
+      <RecentBlogItem/>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+/* eslint-disable no-undef */
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Oswald",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        grey: "#5F7264",
+        olivegreen: "#7EAC8A",
+        red: "#9F6544",
+      },
+    },
   },
   variants: {
     extend: {},
