b85db4ba0e98abcc431d8e5294ec183122330e06
marwaghassa <marwaghassa7@gmail.com>
2021-07-27 17:56:23 +0300

    Home page header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import React from "react";
+
 import HomePageHeader from "./component/Home-page-header/homepageheader";
+
 
 function App() {
   return (
     <div >
+       <p>
+            Welcome to the capstone projects, this is your initial react app
+          </p>
       <HomePageHeader />
     </div>
   );
--- src/component/Home-page-header/homepageheader.jsx
+++ src/component/Home-page-header/homepageheader.jsx
@@ -1,43 +1,14 @@
 import React from "react";
 
-
-
 export default function HomePageHeader(){
 return (
  <div className="flex flex-col place-items-center bg-pink">
- 
-     
-       {/* <div className=" relative">  */}
-      {/* <div className="relative left-20 top-2   shadow-xl"> <img 
-       width="179px" height="282px"src="../images/header1.jpeg" alt="1" /></div>
 
-       <div className="relative left-60  -top-20 "><img width="250" height="311" src="../images/header3.jpeg" alt="3" /></div>
-       <div className="relative  left-20 -top-16 shadow-xl">
-           <img width="130" height="127"
-             src="../images/header2.jpeg" alt="2" /></div>
-       </div> */}
-
-  {/* <div className="static ml-8 mt-5
-           left-12 top-14 shadow-ll"> <img 
-       width="179px" height="282px" src="../images/header1.jpeg" alt="1" /></div>
-
-        <div className=" static  "><img width="250" height="3" src="../images/header3.jpeg" alt="3" /></div>
-       <div className="static w-full  mt-5 ml-5 shadow-l">
-           <img width="198px"
-                height="12"
-             src="../images/header2.jpeg" alt="2" /></div> </div> */}
-       
-
-       <div className="relative h-full w-full left-40 top-40 mt-20 ">
+  <div className="relative h-full w-full left-40 top-40 mt-20 ">
   <img src="../images/header3.jpeg" alt="PRODUCT" className="h-60 w-60 object-cover absolute left-32 top-25 shadow-lg " />
   <img src="../images/header1.jpeg" alt="PRODUCT" className="h-60 w-40 object-cover absolute -left-9 -top-40  shadow-lg shadow-ll " />
   <img src="../images/header2.jpeg" alt="PRODUCT" className="h-40 w-30 object-cover absolute -bottom-80 left-4 shadow-l" /> </div>
   
-  
-
-
-
-
 
     <div className=" flex flex-col  justify-center   static bg-pink ">
 <h1 className='text-oswald text-3xl font-medium text-gray-700 text-center text-red mb-5'>DON’T PANIC, IT’S ORGANIC!</h1> <br/>
