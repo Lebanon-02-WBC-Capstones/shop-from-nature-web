e0aa64ffb97c5ff992f7722b21d1b7471c28febf
osamaakb <osama98akb@gmail.com>
2021-07-27 15:03:51 +0300

    fix: cleaned app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
 import React from "react";
-import NewsLetter from "./components/NewsLetter/NewsLetter";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <header className="App-header">
-          <NewsLetter />
-        </header>
-        <Footer />
-      </Router>
-    </div >
+    <Router>
+      <Navbar />
+      <Footer />
+    </Router>
   );
 }
 
