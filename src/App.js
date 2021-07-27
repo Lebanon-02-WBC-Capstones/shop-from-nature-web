89b7e902206615578004c198adcbb4e972864313
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-07-27 22:41:39 +0300

    fix:remove cartitem from app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import CartItem from "./components/CartItem/CartItem";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Footer />
-      <CartItem />
     </Router>
   );
 }
