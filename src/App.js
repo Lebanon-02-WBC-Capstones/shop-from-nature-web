f87294dde7f9003c8b1c505c2910bfa6c1bd6812
osamaakb <osama98akb@gmail.com>
2021-07-27 17:33:35 +0300

    Fox: remove comments from app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import React from "react";
-import BlogCard from "./components/BlogCard/BlogCard";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import Navbar from "./components/Navbar/Navbar";
-// import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
