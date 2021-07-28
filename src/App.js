f1bbd063be74a85f5a8bf8e6c3f3c6c46fdedf35
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-07-28 19:46:58 +0300

    fix:component in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import React from "react";
-import Footer from "./components/Footer/Footer";
-import OurMission from "./components/OurMission/OurMission";
-import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Footer />
-      <OurMission />
     </Router>
   );
 }
