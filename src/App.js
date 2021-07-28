c28216e2bd3c9d57927ac6199ac474f8b0dfc994
Mohamad Al Sabaa <mohamadsabaa3@gmail.com>
2021-07-28 22:21:02 +0300

    resolve the new arrival Item

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import ArrivalItemsData from "./components/ArrivalItems/ArrivalItemsData";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <ArrivalItemsData/>
       <Footer />
     </Router>
   );
