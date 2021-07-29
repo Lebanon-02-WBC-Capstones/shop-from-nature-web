ad024e460eb7b9cbfb6c9068b9190eb9fbd461ef
marwaghassa <marwaghassa7@gmail.com>
2021-07-29 16:49:52 +0300

    About page header (positions fixed)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Aboutpageheader from "./components/aboutpageheader/aboutpageheader";
+import Aboutpageheader from "./components/aboutpageheader/aboutPageHeader";
 function App() {
   return (
     <Router>
