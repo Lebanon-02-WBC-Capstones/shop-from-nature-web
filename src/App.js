a0ca501bead1959c126379898cd7689b6cc9968e
Mohamad Al Sabaa <mohamadsabaa3@gmail.com>
2021-07-29 16:02:01 +0300

    Feat: shop component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import React from "react";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Route path="/Shop" component={Shop} />
       <Footer />
     </Router>
   );
