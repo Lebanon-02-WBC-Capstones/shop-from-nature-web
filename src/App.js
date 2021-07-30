df25d8d4d70eb3e42190a68e083317d242aeea6e
MiraChami <Miramc149@gmail.com>
2021-07-30 14:50:39 +0300

    style: fix font

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import React from "react";
-// import Navbar from "./components/Navbar/Navbar";
-// import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
-import TeamMemberCard from "./components/TeamMemberCard/TeamMemberCard";
 
 function App() {
   return (
     <Router>
-      
-      <TeamMemberCard />
-     
+      <Navbar />
+      <Footer />
     </Router>
   );
 }
