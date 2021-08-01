d471c404a572c947458f879ca981420b924ffdea
osamaakb <osama98akb@gmail.com>
2021-08-01 14:00:06 +0300

    Fix: position of button and title

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -9,27 +9,25 @@ const BlogCard = ({ blogDate, blogTitle, blogDescription }) => {
   };
 
   return (
-    <div className="flex-col justify-items-start shadow-lg m-4 w-96 h-80">
-      <div className="text-left px-6">
-        <p className=" text-red text-sm font-medium leading-5 pt-6 ">
+    <div className="flex flex-col justify-between items-center shadow-lg m-4 w-96 h-80">
+      <div className="text-left w-full px-8">
+        <p className="text-red text-sm font-medium leading-5 pt-6">
           {blogDate}
         </p>
-        <h1 className=" text-olivegreen text-2xl font-bold leading-7 py-3">
+        <h1 className="text-olivegreen text-2xl font-bold leading-7 py-3">
           {blogTitle}
         </h1>
-        <p className=" text-red text-lg font-bold leading-7 pb-8">
+        <p className="text-red text-lg font-bold leading-7 pb-8">
           {blogDescription}
         </p>
       </div>
-      <div>
-        <button
-          type="button"
-          onClick={blogPage}
-          className=" bg-olivegreen text-center text-white text-sm w-28 h-12 rounded"
-        >
-          Read More
-        </button>
-      </div>
+      <button
+        type="button"
+        onClick={blogPage}
+        className=" bg-olivegreen text-center text-white text-sm w-28 h-12 rounded my-6"
+      >
+        Read More
+      </button>
     </div>
   );
 };
