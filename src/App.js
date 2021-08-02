56197c8a47988d45775bc1bc3079f3f35f3232c7
Mohamad Al Sabaa <72558470+Mohamad-Sabaa@users.noreply.github.com>
2021-08-02 13:28:30 +0300

    Feat: shop header (#34)
    * Feat: shop component
    
    * Feat: the shop header
    
    * center shop text
    
    * center the shop title

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <header className="App-header">
-          <p>
-            Welcome to the capstone projects, this is your initial react app
-          </p>
-        </header>
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Footer />
+    </Router>
   );
 }
 
