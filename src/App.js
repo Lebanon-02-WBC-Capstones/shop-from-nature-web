a01006f5b9bbd7a7eeeff81c6da54390a1de4e10
cesarmaaz <cesarmaaz@gmail.com>
2021-08-04 12:31:33 +0300

    Feat: Blogs Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
-import BlogPage from "./pages/BlogsPage/BlogPage";
 
 function App() {
   return (
