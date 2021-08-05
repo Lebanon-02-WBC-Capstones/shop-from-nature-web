9e6aaabbc55e67eb8ab991d25c2afee54fa4897b
Mohamad Al Sabaa <mohamadsabaa3@gmail.com>
2021-08-05 13:28:26 +0300

    Feat: blog details page

---
+++ src/components/BlogDetailsPage/data.jsx
@@ -0,0 +1,18 @@
+export const data = {
+  title: "All you need to know about bamboo straws | Jungle Straws",
+  subtitle: "Jungle Straw July 1st, 2021 . 5 min read",
+  img1: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1551750250996-KTIVXBN16ZL880GIRCNV/are+bamboo+straws+reusable-can+I+buy+bulk+bamboo+straws.jpg?format=500w",
+  header1: "Bamboo straws impact.",
+  descp1:
+    "Buying bamboo straws from real bamboo stalks that you can reuse over and over again is about as sustainable and planet-friendly as you can get. If we compare the damage that single-use plastics are causing to our coastlines with what happens when bamboo has reached the end of its’ life, there really is no question on which material has the harshest effects on our environment. The good news is that once you have finished with your bamboo straw completely, you can simply toss them onto the compost heap where they will break down and compose naturally.",
+  img2: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1560224554434-APBMB43JEV90822LN9G5/Sustainable+and+Organic+Bamboo+Straws?format=500w",
+  img3: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1551751410702-LXNHCN4F58UK7ZVUG8AO/Bamboo+growing?format=750w",
+  header2: "How are bamboo straws made?",
+  descp2:
+    "Vietnamese farmers will first travel into the bamboo plantations and forests to seek out the bamboo trees which are at full maturity. It is important that the trees picked are of good quality. Once identified and discovered, the stalks are then chopped off and transported back to the farm where they will be allowed to dry naturally under the sunlight.The bamboo stems are then cut from their natural state into lengths suitable for straws. We are a zero waste bamboo straw company, which means that we use all parts of the stalk and the material. This is why bamboo straws differ in thickness; their unique charm being that each straw is completely different! Once the stems have been cut, they then gets sanded, filed and etched to give them a smooth and natural finish.",
+  header3: "Bamboo straws with a logo? Is this possible?",
+  descp3:
+    "Absolutely! Bamboo straws can be branded easily with a company logo or emblem and will look great in every environment. Bamboo straws can be used for restaurants, weddings & events, and most other occasions that you can think of. Branded bamboo straws are not only affordable, but are a great way to show your company or businesses green credentials and can be a great way to market your ideas and the story behind your brand.",
+  img4: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584260363217-2EIE8UG406XWYYWNEBVC/Bamboo+Drinking+Straws+by+Jungle+Straws+Jungle+Culture?format=500w",
+  img5: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584260362741-7BSN67YX4JO91WM6F3FO/Jungle+Straws+Bamboo+Drinking+Straws+Reusbale.jpg?format=750w",
+};
