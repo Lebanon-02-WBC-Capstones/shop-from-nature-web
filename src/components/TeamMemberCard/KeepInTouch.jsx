0e59c509d1ea70134733b0995f33b4818efc8639
MiraChami <Miramc149@gmail.com>
2021-08-05 20:25:13 +0300

    feat: about us page without modal

---
+++ src/components/TeamMemberCard/KeepInTouch.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function KeepInTouch() {
+  return (
+    <div className="flex">
+      <button className="bg-red text-white text-center float-right w-44 py-3 px-6 text-xl rounded-3xl">
+        Keep In Touch
+      </button>
+    </div>
+  );
+}
+
+export default KeepInTouch;
--- src/components/TeamMemberCard/TeamMemberCard.jsx
+++ src/components/TeamMemberCard/TeamMemberCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Member1 from "./../../components/TeamMemberCard/Member1";
 import Member2 from "./../../components/TeamMemberCard/Member2";
 import Member3 from "./../../components/TeamMemberCard/Member3";
@@ -6,15 +6,21 @@ import Member4 from "./../../components/TeamMemberCard/Member4";
 import Member5 from "./../../components/TeamMemberCard/Member5";
 import Member6 from "./../../components/TeamMemberCard/Member6";
 import Member7 from "./../../components/TeamMemberCard/Member7";
+import KeepInTouch from "./KeepInTouch";
 function TeamMemberCard() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="flex flex-col bg-mainbg place-items-center text-center p-10">
-      <p className="text-bric font-sans font-normal text-3xl">
-        People behind our success
-      </p>
+    <div className="flex flex-col bg-mainbg  p-10">
+      <div className="flex flex-row justify-around">
+        <p className="text-bric font-sans float-left font-medium text-4xl ">
+          People behind our success
+        </p>
+        <KeepInTouch key={showModal} onClick={() => setShowModal(true)} />
+      </div>
       <div className="">
         <Member1 />
-        <div className="grid grid-flow-col grid-cols-3 grid-rows-2 gap-20">
+        <div className="grid grid-flow-col grid-rows-2 gap-x-px gap-y-4 ">
           <Member2 />
           <Member3 />
           <Member4 />
