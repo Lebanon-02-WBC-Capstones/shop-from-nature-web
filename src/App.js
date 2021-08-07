cfe9dcad813ab813fb1d293590c2ba7f5e752861
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-08-07 16:58:34 +0300

    setting up firebase with 7 products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import API from "./API";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+  const [prds, setProducts] = useState([]);
+
+  useEffect(() => {
+    API.getProducts().then((products) => {
+      setProducts(products);
+    });
+  }, []);
+  console.log(prds);
   return (
     <Router>
       <Navbar />
