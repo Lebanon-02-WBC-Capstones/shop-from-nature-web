06f5a1f2ece829249d37a6ebb5302aa4b9f0ec33
MiraChami <Miramc149@gmail.com>
2021-08-07 20:40:44 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import About from "../../pages/About";
+import About from "./pages/About/About";
 import { BrowserRouter as Router, Switch,Route } from "react-router-dom";
 
 
