ac19a28540c655877ff4f2ca1adb17ed930c7bf8
osamaakb <osama98akb@gmail.com>
2021-08-07 20:16:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import API from "./API";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  const [prds, setProducts] = useState([]);
 
-  useEffect(() => {
-    API.getProducts().then((products) => {
-      setProducts(products);
-    });
-  }, []);
-  console.log(prds);
   return (
     <Router>
       <Navbar />
