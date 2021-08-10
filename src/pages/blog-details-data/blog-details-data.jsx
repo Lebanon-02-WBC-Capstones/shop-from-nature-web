df3ff6b86b85608420f9483bf76f0241b2a310d1
Mohamad Al Sabaa <mohamadsabaa3@gmail.com>
2021-08-10 17:58:57 +0300

    "Feat: new blog-details-data"

---
+++ src/pages/blog-details-data/blog-details-data.jsx
@@ -0,0 +1,93 @@
+export const data = [
+  {
+    id: 1,
+    title: "All you need to know about bamboo straws | Jungle Straws",
+    subtitle: "Jungle Straw July 1st, 2021 . 5 min read",
+    img1: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1551750250996-KTIVXBN16ZL880GIRCNV/are+bamboo+straws+reusable-can+I+buy+bulk+bamboo+straws.jpg?format=500w",
+    header1: "Bamboo straws impact.",
+    descp1:
+      "Buying bamboo straws from real bamboo stalks that you can reuse over and over again is about as sustainable and planet-friendly as you can get. If we compare the damage that single-use plastics are causing to our coastlines with what happens when bamboo has reached the end of its’ life, there really is no question on which material has the harshest effects on our environment. The good news is that once you have finished with your bamboo straw completely, you can simply toss them onto the compost heap where they will break down and compose naturally.",
+    img2: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1560224554434-APBMB43JEV90822LN9G5/Sustainable+and+Organic+Bamboo+Straws?format=500w",
+    img3: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1551751410702-LXNHCN4F58UK7ZVUG8AO/Bamboo+growing?format=750w",
+    header2: "How are bamboo straws made?",
+    descp2:
+      "Vietnamese farmers will first travel into the bamboo plantations and forests to seek out the bamboo trees which are at full maturity. It is important that the trees picked are of good quality. Once identified and discovered, the stalks are then chopped off and transported back to the farm where they will be allowed to dry naturally under the sunlight.The bamboo stems are then cut from their natural state into lengths suitable for straws. We are a zero waste bamboo straw company, which means that we use all parts of the stalk and the material. This is why bamboo straws differ in thickness; their unique charm being that each straw is completely different! Once the stems have been cut, they then gets sanded, filed and etched to give them a smooth and natural finish.",
+    header3: "Bamboo straws with a logo? Is this possible?",
+    descp3:
+      "Absolutely! Bamboo straws can be branded easily with a company logo or emblem and will look great in every environment. Bamboo straws can be used for restaurants, weddings & events, and most other occasions that you can think of. Branded bamboo straws are not only affordable, but are a great way to show your company or businesses green credentials and can be a great way to market your ideas and the story behind your brand.",
+    img4: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584260363217-2EIE8UG406XWYYWNEBVC/Bamboo+Drinking+Straws+by+Jungle+Straws+Jungle+Culture?format=500w",
+    img5: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584260362741-7BSN67YX4JO91WM6F3FO/Jungle+Straws+Bamboo+Drinking+Straws+Reusbale.jpg?format=750w",
+  },
+
+  {
+    id: 2,
+    title: "HOW TO BE ETHICAL AND SMELL GOOD DOING IT!",
+    subtitle: "Jungle Straw October 3, 2018 . 5 min read",
+    img1: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1538540877299-LYFQ29QMQDC9Z4BO8HTL/Ethical-Deodorant+Sports+Star?format=1500w",
+    header1: "",
+    descp1:
+      "If you had asked me 2 years ago to switch from spray deodorant to balm, I would’ve recoiled. Balm, to many people sounds like quite an unnatural substance to rub under our arm pits… and I thought so too. Until I met my girlfriend Savannah, who is from America. In America spray deodorant is seen as quite bizarre or disgusting by many. Every time I sprayed my deodorant Savannah would hide her nose under her t-shirt like I was releasing anthrax. It definitely wasn’t giving me the desired… Lynx effect.",
+    img2: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1538540944079-UHCTV5J0Y5M7HRPJ7FVS/Sweat+for+active+beauty?format=1500w",
+    img3: "https://cremocompany.com/media/catalog/product/cache/b07aaa6e8dd0d25ff8a34818b74d03ff/b/o/bodeoscent.jpeg",
+    header2: "",
+    descp2:
+      "Across the pond they use roll-ons mostly. Which to me was quite strange because I was under the impression that roll-on deodorant didn’t work that well…. I also have vivid nightmares of kids at my school passing the roll-on around the boys changing room, classmates would one after the other rub it in their hairy pits. Surely this can’t be the hygienic solution can it? As consumers we are easily led to believe one thing by powerful marketing that just isn’t true. If 100 women chase a man around an island on the TV because he is wearing Lynx Africa it must be incredible mustn’t it? But the reality is far from it. Lynx and other deodorants smell okay. They work well enough too. But research from the journal of science in early 2018 has shown that petroleum-based chemicals used in perfumes, paints and other consumer products can, taken together, emit as much air pollution in the form of volatile organic compounds, or V.O.C.s, as motor vehicles do. Not only that, the packaging from these products is less than often recycled and usually ends up in landfills or oceans.",
+    header3: "WHY NOT GIVE NATURAL DEODORANT A TRY?",
+    descp3:
+      "Natural deodorant is absolutely fantastic. The fragrances are incredible and the packaging is completely recyclable. A 60ml tub can last a reasonably smelly person at least 3 months and they are strong enough to quell even the most gruesome odours. I personally use a deodorant balm by Procoal that is charcoal activated and completely aluminium free. Another fun fact… the cold cream feels pretty nice under your pits! My advice. Ditch the noxious sprays and test out something less intrusive for a few months. What do you have to lose?",
+    img4: "http://cdn.shopify.com/s/files/1/0062/3689/7347/products/New_Botanik_fresh_evergreen-front_1200x1200.jpg?v=1549326832",
+    img5: "https://api.time.com/wp-content/uploads/2016/02/deodorant.jpg?w=824&quality=70",
+  },
+
+  {
+    id: 3,
+    title: "ZERO WASTE BACKPACKING TIPS",
+    subtitle: "Jungle Straw July 26, 2020 . 5 min read",
+    img1: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1537938296902-9PTA0OFF36P6ZK3HW16N/Zero-Waste-Tips-For-Backpackers-Jungle-Straws-Bamboo-Straws-UK.jpg?format=1500w",
+    header1: "",
+    descp1:
+      "As a Westerner, backpacking is easy…. We arrive, we make friends, we eat cheap food, drink cheap beer and sleep in cheap beds. We sit around discussing the places we’ve visited with relative strangers over a Chang or a Bintang, and next week we’re in a different place We eat Pad Thai on the street on the infamous Kao San Road, spicy noodles in Hanoi, and binge on tacos in Mexico City.... and then we leave. We leave, but the waste stays. Backpacking is indeed easy… but the sustainably of backpacking is hard. It seems so inconvenient that we tend to convince ourselves that it’s impossible, and in turn grant ourselves immunity from any responsibility about thinking consciously about our waste. What happens to the Pad Thai forks? The taco boxes? The water bottles?....Are we as backpackers responsible, or is it too much to ask of us to carry bamboo utensils and water purifiers? The honest answer is, that I really don’t know…. Because I have in the past (and sometimes in the present) been guilty of the sin that is ‘backpacking waste’ myself, and it’s something that I am not proud of. So the purpose of this blog post is simple. I can’t tell you what is too much to ask, but I can try to make that ‘ask’ a little bit easier by giving you some handy zero-waste backpacking tips based on my own experiences.",
+    img2: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1537940146359-PVJWII9R50M2VDJO1ZA1/Life+Straw?format=1000w",
+    img3: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1537940445282-IBTEIB43BB4PJIL6OF0B/Buy-Reusable-Bamboo-Straws-UK-Jungle-Straws-Organic-Ethical-Product.png?format=1500w",
+    header2: "GOING ZERO WASTE TRAVEL ",
+    descp2: "My biggest tip…. Travel water purifiers. The one I carry with me is by a company called ‘LifeStraw’ It is extremely portable and it literally allows you to drink from a pond (if you wanted to!). I do a lot of trekking, so it’s the perfect companion for me. There are many variants available, so if you are going to be in a country where you know tap water is potentially ‘diarrhoea threatening’, buy a water purifier in the form of a bottle! They are compact, travel friendly and don’t cost a lot. If you consider that you’ll otherwise be spending at least £1 a day on water from local stores and wasting tons of plastic bottles in the meantime, then buying one goes a long way in reducing your single-use plastic consumption.",
+    header3: "BAMBOO STRAWS?",
+    descp3: "If you pack smart, a small kit like this will be tiny, and super easily to fit into your luggage. It gives you somewhere to easily put left-over food and you’ll never need to hold a street food container again. Simple. Easy. Zero-Waste! ",
+    img4: "https://cdn.shopify.com/s/files/1/0234/7341/products/Bamboo-Straws-1_800x.jpg?v=1563568600",
+    img5: "https://cdn.shopify.com/s/files/1/0382/5249/products/bamboo-straws-display_9.23.10_am_2048x.jpg?v=1573700890",
+  },
+  {
+    id: 4,
+    title: " 3 REASONS YOU SHOULD BE TALKING ABOUT BAMBOO STRAWS",
+    subtitle: "Jungle Straw March 15, 2020 . 5 min read",
+    img1: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584266049051-SGQQWKI0OSKXN3DV8959/Jungle+Straws+Bamboo+Straw+Pouches+in+multiple+colours.jpg?format=500w",
+    header1: "1. BAMBOO STRAWS ARE COOL & THE PLASTIC ERA IS OVER…",
+    descp1:
+      "The single-use plastic ban is sweeping its way across Europe as we speak. By 2021, plastic straws will be no more and going plastic-free is going to be at the forefront of everyone’s mind. Over the coming months, businesses and restaurants will be taking action to find an alternative to plastic straws. That might be bamboo, or even paper or metal. Get ahead of the phasing-out stage and start spreading the word about environmentally friendly options now. Find out what your local coffee shop or favourite bar is planning to do and point them in the right direction of where they can buy recyclable straws. ",
+    img2: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584260362741-7BSN67YX4JO91WM6F3FO/Jungle+Straws+Bamboo+Drinking+Straws+Reusbale.jpg?format=750w",
+    img3: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584260642488-NODE7ZMV3OQOPOYDPT4B/Metal+Drinking+Straws+by+Jungle+Straws+Eco+Straws.jpg?format=750w",
+    header2: "2. …BUT THE ENVIRONMENT IS STILL AT RISK ",
+    descp2: "We know you don’t need us to tell you about the crisis that is climate change. Plastic plays a very guilty role in global warming, from its production to the dreadful way in which it is handled when it loses it purpose. By switching to a reusable bamboo straw or an alternative to plastic straw with a much lower carbon footprint and encouraging others to join you, you’re doing your bit for the environment. It might feel small, but the more people who make the change and talk about it - the more difference we can make. ",
+    header3: "3. ARE BAMBOO STRAWS SAFE & ETHICAL?",
+    descp3: "Can bamboo straws be recycled and why are bamboo straws good for the environment when compared to plastic? These are questions we often get asked, and we're always delighted to tell people that our bamboo straws are completely free from chemicals, pesticides and other nasties. This makes them super safe to drink out of. They’re great for kids too - you can even find bamboo straws thick enough to squeeze through the thickest of milkshakes, or a boba tea with those delightful tapioca bubbles that the little ones dream of! Oh, and in case you wondered, bamboo straws impose no risk to the environment as long as they are harvested sustainably. Zero. For example, all of our Jungle Culture straws are sourced responsibly from bamboo plantations where bamboo grows in abundance. That means there’s no risk of deforestation! ",
+    img4: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584264058512-WBJ1QJ4A1NH0E2N2X5VN/Jungle+Straws+Bamboo+Straws.jpg?format=500w",
+    img5: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1584264098247-0STI1SLOHG1YQSD8X0Q3/Screenshot+2020-02-17+at+17.27.02.png?format=750w",
+  },
+  {
+    id: 5,
+    title: "FINDING BALANCE BETWEEN MINIMALISM AND LIVING A ZERO-WASTE LIFESTYLE",
+    subtitle: "Jungle Straw September 26, 2018 . 5 min read",
+    img1: "https://images.squarespace-cdn.com/content/v1/5b277af875f9ee04f69775de/1537928347381-AT49IGPAFOJK1NE2TEYC/Minimalism-Zero-Waste-Man+Walking?format=1500w",
+    header1: "HOW TO BE A SUSTAINABLE MINIMALIST",
+    descp1:
+      "As a minimalist, I carry most of my worldly possessions in an Osprey 40L backpack. I own 5 t-shirts and 3 pairs of trousers. Whilst I don’t like clutter, and I try to avoid wasteful purchases, I am sometimes gripped by the undeniable pull of consumerism.  At the same time, I am aspiring to be as zero-waste as possible. Naturally, I’m trying to lower my plastic consumption by using alternatives to plastic. I won’t leave my house without a stainless steel water bottle and my reusable bamboo straw. I’ve ditched the disposable razor in exchange for an eco-friendly bamboo safety razor. But, you can see what I’m doing. By making the switch to earth-friendly products, I’m...consuming.  I’m shopping for reusable straws and fancy stainless steel bottles with pictures of avocados on the side of them.  So the questions are simple; are these two philosophies inherently opposed? Is there a balance between being a minimalist and being sustainable? Am I still a minimalist if I own a £40 triple-walled stainless steel consumer bottle? Since when did finding the balance between minimalism and living a zero-waste lifestyle become so complex?",
+    img2: "https://i0.wp.com/thecircularconsumer.com/wp-content/uploads/2021/06/nenad-radojcic-RF5U8BkaQHU-unsplash.jpg?resize=720%2C471&ssl=1",
+    img3: "https://images.unsplash.com/photo-1552242718-c5360894aecd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNTcwODk2fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+    header2: "HOW TO BE A SUSTAINABLE MINIMALIST ",
+    descp2: "The reality is that the definition of minimalism varies from person to person, and the “zero-waste life” is a process: a journey. Despite this, it is still tempting to self-loathe every time we buy a bottle of water on a hot day. Ditto when we sip a cocktail through a single-use plastic straw in a bar. But whilst we shouldn’t be ashamed of our waste; we should be conscious of it. I am still on my journey of learning how to be a sustainable minimalist. I am still actively on the lookout for products that fit into the intersection of my minimalism and sustainability ‘Venn diagram.’",
+    header3: "LET’S TALK ZERO-WASTE ",
+    descp3: "But as I said before: the zero-waste lifestyle is a journey. So share your ideas below, and tell me what fits into your ‘Venn diagram.’ Are you struggling with finding a balance between minimalism and living a zero-waste lifestyle? Let us know and perhaps another member of the Jungle Straws community has the perfect zero-waste yet minimalist swap! We’d also love to hear of any sustainable products that you’d like to see a business such as us launch. ",
+    img4: "https://i1.wp.com/thecircularconsumer.com/wp-content/uploads/2021/02/jon-flobrant-_r19nfvS3wY-unsplash-scaled.jpg?resize=720%2C471&ssl=1",
+    img5: "https://mymodernmet.com/wp/wp-content/uploads/2021/03/black-and-white-photography-contest-sit-down-free-thoughts-by-pierre-pellegrini.jpg",
+  },
+];
