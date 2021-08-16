10a1ab0fabfa1922bf96f906bc1a9fec34351e7c
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-08-16 20:20:59 +0300

    fix: submitting is now working!

--- src/components/contactUsModel/ThankYouModal.jsx
+++ src/components/contactUsModel/ThankYouModal.jsx
@@ -1,31 +1,26 @@
-import React,{useState} from "react";
-import Leaf2 from "../images/Leaf2";
-export default function Modal() {
-  const [showModal, setShowModal] = useState(false);
+import React from "react";
+import Leaf2 from "../../images/Leaf2";
+export default function Modal({ show, setShow }) {
+  // const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button
-        className="bg-red text-white  font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Submit
-      </button>
-      {showModal ? (
+      {show ? (
         <>
-          <div onClick={() => setShowModal(false)}
+          <div
+            onClick={() => setShow(false)}
             className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
             <div className=" relative w-auto my-6 mx-auto max-w-3xl">
-           
-              <div  className="bg-mainbg border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-               
+              <div className="bg-mainbg border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex flex-col items-start justify-between p-5   rounded-t">
-                  <h3 className= "w-full inline-flex justify-center  text-6xl text-red ml-4">
-                   Thank You Name.
-                  </h3> <br/>
-                  <h4 className="w-full inline-flex justify-center text-red ml-4 text-2xl">We will be in touch shortly!</h4>
+                  <h3 className="w-full inline-flex justify-center  text-6xl text-red ml-4">
+                    Thank You For Contacting Us
+                  </h3>{" "}
+                  <br />
+                  <h4 className="w-full inline-flex justify-center text-red ml-4 text-2xl">
+                    We will be in touch shortly!
+                  </h4>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -35,10 +30,9 @@ export default function Modal() {
                     </span>
                   </button>
                 </div>
-               
+
                 <div className="relative -mt-20 -pt-6  flex-auto">
-                  <Leaf2 className="" /> 
-               
+                  <Leaf2 className="" />
                 </div>
               </div>
             </div>
